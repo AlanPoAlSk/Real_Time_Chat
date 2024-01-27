a60c35a4d8853e6cb0feaee230b1d8f80510de84
AlanPoAlsk <alsk91@gmail.com>
2024-01-26 20:00:15 -0500

    add correction to last messages error

--- client/src/hooks/useFetchLastMessage.jsx
+++ client/src/hooks/useFetchLastMessage.jsx
@@ -5,27 +5,35 @@ import { baseUrl, getRequest } from "../utils/service";
 export const useFetchLastMessage = (chat) => {
     const { newMessage, notifications } = useContext(ChatContext);
     const [lastMessage, setLastMessage] = useState(null);
-    const [error, setError] = useState(null); // Add a state for error
+    const [loading, setLoading] = useState(true); // Add loading state
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         const getMessages = async () => {
             try {
+                setLoading(true); // Set loading to true while fetching data
+                setError(null); // Reset error state
+
                 const response = await getRequest(`${baseUrl}/messages/${chat._id}`);
 
                 if (response.error) {
                     setError(response.message); // Set the error state
-                    return console.log('Error!!!', response.message);
+                    return console.error('Error!!!', response.message);
                 }
 
                 const lastUserMessage = response[response?.length - 1];
                 setLastMessage(lastUserMessage);
             } catch (error) {
                 setError(error.message); // Set the error state
-                console.log('Error!!!', error.message);
+                console.error('Error!!!', error.message);
+            } finally {
+                setLoading(false); // Set loading to false after fetching data (success or error)
             }
         };
 
         getMessages();
     }, [newMessage, notifications]);
 
+    return { lastMessage, loading, error }; // Return loading state along with lastMessage and error
 };
+
